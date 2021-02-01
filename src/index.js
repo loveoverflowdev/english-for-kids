b3b8a05420ba84cd7d331eba0db4a304f36cb610
Boris Krasko <boris.krasko@icloud.com>
2021-02-01 20:17:00 +0100

    init: add project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './js/main.js';
+import './sass/main.scss';
+import './sass/button.scss';
+import './css/style.css';
+import './css/flip-card.css';
+import './css/menu.css';
+require.context("./images/", true, /\.(png|jpg|gif)$/);
+require.context("./svg/", true, /\.svg$/);
+require.context("./audio/", true, /\.mp3$/);
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import './app.js';
+
+document.body.style.background= "url('images/bg_looney_blur.jpg')";
+document.body.style.backgroundRepeat = 'no-repeat';
+document.body.style.backgroundSize = 'cover';
